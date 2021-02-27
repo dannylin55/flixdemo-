fa684cb01cec440ee43336a630315dd9277411e2
dannylin55 <71365699+dannylin55@users.noreply.github.com>
2021-02-27 00:03:20 -0500

    Add files via upload

---
+++ flixdemo/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixdemo
+//
+//  Created by Danny Lin on 2/25/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
